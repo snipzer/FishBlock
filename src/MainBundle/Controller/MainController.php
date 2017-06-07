76aeecda848e33e29aa1c5809694998b279b469c
snipzer <l.arif@laposte.net>
2017-06-07 17:07:40 +0200

    Création de la structure de base début

--- src/MainBundle/Controller/MainController.php
+++ src/MainBundle/Controller/MainController.php
@@ -12,33 +12,97 @@ class MainController extends Controller
     /**
      * Page d'accueil
      */
-    public function homeAction(Request $request)
+    public function accueilAction(Request $request)
     {
-        return $this->render("MainBundle:Main:home.html.twig");
+        /**
+         * TODO:
+         * Login (UserRepository)
+         * New User (EntityManager)
+         * Recupération des séries populaires (SerieRepository)
+         * Switch de la langue
+         */
+
+        return $this->render("MainBundle::home.html.twig");
     }
 
-    /**
-     * Wall
-     */
     public function wallAction(Request $request)
     {
-        return $this->render("MainBundle:Main:wall.html.twig");
+        /**
+         * TODO:
+         * Information série (SerieRepository)
+         * Note série (SerieRepository)
+         * Ajout série en favoris (FavoriteRepository)
+         * Suggestion de série (SerieRepository)
+         * Afficher les critiques des séries que l'utilisateur à en favoris (Service)
+         * Système de like/dislike (CriticNotationRepository)
+         */
+
+        return $this->render("MainBundle::wall.html.twig");
     }
 
-    /**
-     * Affichage de toutes les séries que l'utilisateur a dans ses favoris
-     */
+    public function unloggedWallAction(Request $request)
+    {
+        /**
+         * TODO:
+         * Service de notification (Service)
+         * Récupération des séries populaires(SerieRepository)
+         * Récupération des derniers épisodes publier (EpisodeRepository)
+         * Récupération des dernières série publier (SerieRepository)
+         * Récupération des dernières critiques (CriticRepository)
+         */
+
+        return $this->render("MainBundle::unloggedWall.html.twig");
+    }
+
+
+    public function searchAction(Request $request)
+    {
+        /**
+         * TODO:
+         * Récupération des séries (SerieRepository)
+         * Process rechercher des séries (Service)
+         *      Récupérer les séries en base de données avec la recherche
+         *      SI pas de résultat: Récupére la série avec tvdb (Service TvdbConnector)
+         *             Si pas de résultat on propose de créer la série
+         *      Si un résultat l'envoyer sur la page
+         * Ajout de série en favoris (FavorisRepository)
+         *
+         */
+
+        return $this->render("MainBundle::search.html.twig");
+    }
+
     public function favorisAction(Request $request)
     {
-        return $this->render("MainBundle:Main:favoris.html.twig");
+        /**
+         * TODO:
+         * Suggestion de série (SerieRepository)
+         * Ajout de série en favoris (FavorisRepository)
+         * Service de notification (Service)
+         * Récupération de la note de la série (SerieRepository)
+         */
+
+        return $this->render("MainBundle::favoris.html.twig");
     }
 
-    /**
-     * Serie
-     */
     public function serieAction(Request $request)
     {
-        return $this->render("MainBundle:Main:serie.html.twig");
+        /**
+         * TODO:
+         * Récupérer les acteurs (ActorRepository)
+         * Récupérer les types (TypeRepository)
+         * Récupérer une/plusieurs série en fonction d'un submit utilisateur (SerieRepository)
+         * Récupérer les épisodes d'une série (EpisodeRepository)
+         * Créer une critique en fonction d'un submit utilisateur (EntityManager)
+         * Modification d'une série avec un submit utilisateur (SerieRepository)
+         * Système de like/dislike (CriticNotationRepository)
+         * Récupération de toutes les critiques d'une série (CriticRepository)
+         * Ajout de la série en favoris (FavorisRepository)
+         * Récupération de la note d'une série (SerieRepository)
+         * Notification (Service)
+         */
+
+        return $this->render("MainBundle::serie.html.twig");
     }
 
 }
