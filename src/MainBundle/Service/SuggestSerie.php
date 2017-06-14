a5050b5fa1c079f5b56a6c215b1a4a413e9d4b67
snipzer <l.arif@laposte.net>
2017-06-14 14:23:09 +0200

    -Fix problem when a serie has no Types

--- src/MainBundle/Service/SuggestSerie.php
+++ src/MainBundle/Service/SuggestSerie.php
@@ -28,32 +28,51 @@ class SuggestSerie extends  Controller
             array_push($array, $userFav->getSerie());
         }
 
-        // On sélectionne une série parmis toutes celles stocker
-        $chosenSerie = $array[array_rand($array)];
+        $bool = true;
 
-        // On stocke les informations de la série de départ
-        $result[] = $chosenSerie;
-
-        // On récupère les type de la série choisie
-        $arraySerieTypes = $chosenSerie->getSerieTypes();
-
-        // On en choisie un au hazard
-        $chosenSerieType = $arraySerieTypes[array_rand($arraySerieTypes)];
-
-        // On récupère sont nom
-        $typeName = $chosenSerieType->getType()->getName();
-
-        // A partir du nom on récupère les sériesType qui ont le même type
-        $serieTypes = $this->manager
-            ->getRepository("MainBundle:SerieType")
-            ->getSeriesTypeByType($typeName);
-
-
-        // Pour chaque sérieType
-        foreach($serieTypes as $serieType)
+        while($bool)
         {
-            if($serieType->getSerie() !== $chosenSerie)
-                array_push($result, $serieType->getSerie());
+            // On sélectionne une série parmis toutes celles stocker
+            $chosenSerie = $array[array_rand($array)];
+
+            // On stocke les informations de la série de départ
+            $result[] = $chosenSerie;
+
+            // On récupère les type de la série choisie
+            $arraySerieTypes = $chosenSerie->getSerieTypes();
+
+            if(count($arraySerieTypes) === 0)
+            {
+                continue;
+            }
+            else
+            {
+                var_dump("Taille du tableau: ". count($arraySerieTypes));
+                echo "<br>";
+                $rand = array_rand($arraySerieTypes);
+
+                var_dump("Index choisie: ".$rand);
+
+                // On en choisie un au hazard
+                $chosenSerieType = $arraySerieTypes[$rand];
+
+                // On récupère sont nom
+                $typeName = $chosenSerieType->getType()->getName();
+
+                // A partir du nom on récupère les sériesType qui ont le même type
+                $serieTypes = $this->manager
+                    ->getRepository("MainBundle:SerieType")
+                    ->getSeriesTypeByType($typeName);
+
+
+                // Pour chaque sérieType
+                foreach($serieTypes as $serieType)
+                {
+                    if($serieType->getSerie() !== $chosenSerie)
+                        array_push($result, $serieType->getSerie());
+                }
+                $bool = false;
+            }
         }
 
         return $result;
