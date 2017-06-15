2dd5ed67186f6a267f2a01d14093429d521d4434
snipzer <l.arif@laposte.net>
2017-06-15 18:25:03 +0200

    Deleting outdated comments

--- src/MainBundle/Controller/MainController.php
+++ src/MainBundle/Controller/MainController.php
@@ -13,12 +13,13 @@ class MainController extends Controller
      */
     public function homeAction(Request $request)
     {
-        /**
-         * TODO:
-         * Login (UserRepository)
-         * New User (EntityManager)
-         * ~~Recupération des séries populaires (SerieRepository)
-         */
+        $boolTrue = $this->getDoctrine()
+            ->getRepository("MainBundle:Favoris")
+            ->checkIfSerieIsInFav("efc837fa-7e98-4543-bbde-df95bb4f8c41", "52fc83e7-59b2-45d3-94c2-c674385afdbe");
+
+        $boolFalse = $this->getDoctrine()
+            ->getRepository("MainBundle:Favoris")
+            ->checkIfSerieIsInFav("123f3c71-8462-4a24-9cb6-1c8152149edf", "52fc83e7-59b2-45d3-94c2-c674385afdbe");
 
 
         $popularSeries = $this->getDoctrine()->getRepository("MainBundle:Critic")->getPopularSerie();
@@ -30,16 +31,6 @@ class MainController extends Controller
 
     public function wallAction(Request $request)
     {
-        /**
-         * TODO:
-         * Information série (SerieRepository)
-         * Note série (SerieRepository)
-         * Ajout série en favoris (FavoriteRepository)
-         * Suggestion de série (SerieRepository)
-         * ~~Afficher les critiques des séries que l'utilisateur à en favoris (Service)
-         * Système de like/dislike (CriticNotationRepository)
-         */
-
         $userId = $this->getUser()->getId()->__toString();
         $user = $this->getUser();
 
@@ -55,15 +46,6 @@ class MainController extends Controller
 
     public function unloggedWallAction(Request $request)
     {
-        /**
-         * TODO:
-         * Service de notification (Service)
-         * Récupération des séries populaires(SerieRepository)
-         * Récupération des derniers épisodes publier (EpisodeRepository)
-         * Récupération des dernières série publier (SerieRepository)
-         * Récupération des dernières critiques (CriticRepository)
-         */
-
         $serieId = "123f3c71-8462-4a24-9cb6-1c8152149edf";
 
         $trendingSerie = $this->getDoctrine()->getRepository("MainBundle:Critic")->getPopularSerie();
@@ -85,18 +67,6 @@ class MainController extends Controller
 
     public function searchAction(Request $request)
     {
-        /**
-         * TODO:
-         * Récupération des séries (SerieRepository)
-         * Process rechercher des séries (Service)
-         *      Récupérer les séries en base de données avec la recherche
-         *      SI pas de résultat: Récupére la série avec tvdb (Service TvdbConnector)
-         *             Si pas de résultat on propose de créer la série
-         *      Si un résultat l'envoyer sur la page
-         * Ajout de série en favoris (FavorisRepository)
-         *
-         */
-
         $user = $this->getUser();
         $series = $this->getDoctrine()->getRepository("MainBundle:Serie")->getSeriesSortByDate();
 
@@ -108,14 +78,6 @@ class MainController extends Controller
 
     public function favorisAction(Request $request)
     {
-        /**
-         * TODO:
-         * Suggestion de série (SerieRepository)
-         * Ajout de série en favoris (FavorisRepository)
-         * Service de notification (Service)
-         * Récupération de la note de la série (SerieRepository)
-         */
-
         $serieId = $request->attributes->get("idSerie");
         $userId = $this->getUser()->getId()->__toString();
         $user = $this->getUser();
@@ -139,20 +101,6 @@ class MainController extends Controller
 
     public function serieAction(Request $request)
     {
-        /**
-         * TODO:
-         * ~~Récupérer les acteurs (ActorRepository)
-         * ~~Récupérer les types (TypeRepository)
-         * ~~Récupérer les épisodes d'une série (EpisodeRepository)
-         * Créer une critique en fonction d'un submit utilisateur (EntityManager)
-         * Modification d'une série avec un submit utilisateur (SerieRepository)
-         * Système de like/dislike (CriticNotationRepository)
-         * ~~Récupération de toutes les critiques d'une série (CriticRepository)
-         * Ajout de la série en favoris (FavorisRepository)
-         * Récupération de la note d'une série (SerieRepository)
-         * Notification (Service)
-         */
-
         $serieId = $request->attributes->get("idSerie");
 
         $EpisodeRepository = $this->getDoctrine()->getRepository("MainBundle:Episode");
@@ -178,11 +126,6 @@ class MainController extends Controller
 
     public function episodeAction(Request $request)
     {
-        /**
-         * TODO:
-         * Pareil que serieAction
-         * Récupérer les informations d'un épidose (EpisodeRepository)
-         */
         $serieId = $request->attributes->get("idSerie");
         $episodeId = $request->attributes->get("idEpisode");
 
@@ -212,15 +155,6 @@ class MainController extends Controller
 
     public function accountAction(Request $request)
     {
-        /**
-         * TODO:
-         * ~~Récupérer les informations de l'utilisateur
-         * Après soumission du formulaire enregistrer les nouvelles informations de l'utilisateur
-         * ATTENTION !
-         * Les mots de passe ne doivent pas être envoyer en clair !
-         * ~~Suggestion de serie (SerieRepository)
-         */
-
         $userId = $this->getUser()->getId()->__toString();
 
         $serieSuggest = $this->get("SuggestSerie")->getSuggestion($userId);
@@ -234,11 +168,6 @@ class MainController extends Controller
 
     public function legalAction(Request $request)
     {
-        /**
-         * TODO:
-         * Afficher les mentions légales du site
-         */
-
         return $this->render("MainBundle:App:legal.html.twig");
     }
 
